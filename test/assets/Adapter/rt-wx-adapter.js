71ec13edb3b129a2a90deb6838aa8d5197e6e6ad
zhiyang.zhang <928926917@qq.com>
2018-11-30 09:31:31 +0800

    [adjust] modify two callback function

--- test/assets/Adapter/rt-wx-adapter.js
+++ test/assets/Adapter/rt-wx-adapter.js
@@ -112,11 +112,11 @@ var InnerAudioContext = function () {
         this.src = null;
         this._audioId = undefined;
         this._endCb = null;
+        this._WaitingCb = null;
         this._inLoop = false;
         this._inVolume = 1.0;
         this._inAutoplay = false;
         this._isStop = false;
-        this._isWaiting = false;
         this._isSeeking = false;
         this._isSeeked = false;
         this._PLAYING = 1;
@@ -163,6 +163,12 @@ var InnerAudioContext = function () {
                         _onFunctionCallback(cbArray2);
                     }
 
+                    var cbArray3 = _getFunctionCallbackArray("onError");
+                    if (cbArray3 !== undefined && audioEngine.getState(this._audioId) === -1) {
+                        var res = { errMsg: "Network error", errCode: 10002 };
+                        _onFunctionCallback(cbArray3, res);
+                    }
+
                     this._beginUpdateProgress();
                 } else if (this._audioId !== undefined && this.loop === false && audioEngine.getState(this._audioId) !== this._PLAYING) {
                     this._audioId = undefined;
@@ -173,6 +179,12 @@ var InnerAudioContext = function () {
                     this._beginUpdateProgress();
                 } else {
                     return;
+                }
+            }
+
+            if (this._audioId !== undefined) {
+                if (this._WaitingCb !== null) {
+                    audioEngine.setWaitingCallback(this._audioId, this._WaitingCb);
                 }
             }
 
@@ -332,16 +344,21 @@ var InnerAudioContext = function () {
     }, {
         key: "onWaiting",
         value: function onWaiting(callback) {
-            if (this._audioId === undefined && this._isWaiting) {
-                callback();
-                return;
+            if (this._WaitingCb === null) {
+                this._WaitingCb = callback;
+                if (this._audioId !== undefined) {
+                    audioEngine.setWaitingCallback(this._audioId, this._WaitingCb);
+                } else {
+                    console.warn("InnerAudioContext onWaiting: currently is no music");
+                }
             }
-            _pushFunctionCallback("onWaiting", callback);
         }
     }, {
         key: "offWaiting",
         value: function offWaiting(callback) {
-            _removeFunctionCallback("onWaiting", callback);
+            if (this._WaitingCb !== null) {
+                this._WaitingCb = null;
+            }
         }
     }, {
         key: "onSeeking",
